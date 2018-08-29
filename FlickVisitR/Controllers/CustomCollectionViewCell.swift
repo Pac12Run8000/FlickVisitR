a60e25079d4dc966e9474497bf6709a54615f3b9
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2018-08-29 14:51:56 -0700

    feat: Added a custom collectionViewCell file for the location photos.

---
+++ FlickVisitR/Controllers/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  FlickVisitR
+//
+//  Created by Michelle Grover on 8/29/18.
+//  Copyright © 2018 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
